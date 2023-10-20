61e8e0d064c704f2c1f6615fd3bdead69610d5a3
xingfei.li 李星飞 <xingfei.li@qiyuesuo.com>
2023-10-20 18:42:25 +0800

    测试配置

--- src/main/java/top/lixxing/web/server/Startup.java
+++ src/main/java/top/lixxing/web/server/Startup.java
@@ -1,14 +1,20 @@
 package top.lixxing.web.server;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import top.lixxing.web.server.config.bean.Server;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Startup {
 
 	private static final Logger logger = LoggerFactory.getLogger(Startup.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		SimpleWebServer simpleWebServer = new SimpleWebServer();
 
@@ -17,5 +23,13 @@ public class Startup {
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		}
+
+		InputStream inputStream = Startup.class.getClassLoader().getResourceAsStream("conf/config.json");
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		Server server = objectMapper.readValue(inputStream, new TypeReference<Server>() {
+		});
+
 	}
 }
